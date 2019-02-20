2354fb3a8eac16039f2935a4671cd8e26acd55ba
karim <karimkuku@gmail.com>
2019-02-20 17:51:57 +0100

    sender device1 should be completed

---
+++ Device1/main.py
@@ -0,0 +1,24 @@
+import time
+import pycom as pyc #might not be correct import!
+
+isRunning = True
+isLightOn = False 
+ledColor = ""
+
+while (isRunning):
+    print('*** Main Menu ***')
+
+    if(isLightOn == True):
+        print("To change the color write the color value in HEX")
+
+    print("To turn on light type 'on'")
+    print("To turn off light type 'off'")
+    userInput = input()
+    if(userInput == "on"):
+        pyc.rgbled('0xFFFFFF')
+    if(userInput == 'off'):
+        pyc.rgbled('0x000000')
+    if(userInput == 'exit'):
+        isRunning = False
+    else:
+        pyc.rgbled(ledColor)
