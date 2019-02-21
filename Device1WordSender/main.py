5bdd7cc0d8200db836037914a7d98ddfd3879c30
karim <karimkuku@gmail.com>
2019-02-21 09:55:22 +0100

    docs

--- Device1WordSender/main.py
+++ Device1WordSender/main.py
@@ -4,23 +4,26 @@ import binascii
 
 #This used to send ascii carraktor to an ambient ligte sensor 
 
+# Method to encode the text to binary
 def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-
+#turns light on for one sec
 def LightOnOneSec():
     pyc.rgbled('0xFFFFFF')
     time.sleep(1)
-
+#turns light off for one sec
 def LightOffOneSec():
     pyc.rgbled('0x000000')
     time.sleep(1)
 
+#Sends the startConnection pattern
 def InitiateConnection():
     pyc.rgbled('0xFFFFFF')
     time.sleep(5)
 
+#Send end of transsmission signal
 def SignalEndText():
     pyc.rgbled('0x000000')
     time.sleep(8)
