d4a9e5e30da1864e1d0bda47452f63a46229cdb5
karim <karimkuku@gmail.com>
2019-02-21 22:18:30 +0100

    change methode name

--- Device1WordSender/main.py
+++ Device1WordSender/main.py
@@ -16,13 +16,11 @@ def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
     return zfill(bits, 8 * ((len(bits) + 7) // 8))
 
 #turns light on for one sec
-def LightOnOneSec():
+def LightOn():
     pyc.rgbled(0xFFFFFF)
-    time.sleep(1)
 #turns light off for one sec
-def LightOffOneSec():
+def LightOff():
     pyc.rgbled(0x000000)
-    time.sleep(1)
 
 #Sends the startConnection pattern
 def InitiateConnection():
@@ -34,6 +32,9 @@ def SignalEndText():
     pyc.rgbled(0x000000)
     time.sleep(8)
 
+def Wait():
+    time.sleep(1)
+
 pyc.heartbeat(False)
 isRunning = True
 ledColor = ""
@@ -42,18 +43,21 @@ while (isRunning):
     print('*** Main Menu ***')
     print("Type the text that you want to send: ")
     print("Type 'Exit' to EndProgram")
-    input()
     text = input()
     if(text == 'Exit'):
         break
-    bitString = text_to_bits(text)
-    print(bitString)
-    InitiateConnection()
-    for bit in str(bitString):
-        if(bit == "0"):
-            LightOffOneSec()
-        elif bit == "1":
-            LightOnOneSec()
-    SignalEndText()
+    if(text != ""):
+        bitString = text_to_bits(text)
+        print(bitString)
+        InitiateConnection()
+        for bit in str(bitString):
+            print("sending: " + bit)
+            if(bit == "0"):
+                LightOff()
+                Wait()
+            elif bit == "1":
+                LightOn()
+                Wait()
+        SignalEndText()
 
 
